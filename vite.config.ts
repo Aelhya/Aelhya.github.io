5c7b4a16415837d54856bb25cd258f32d6edaa7d
adelaidelouis <adelaide.louis78@hotmail.fr>
2024-11-13 09:28:18 +0100

    déploiement

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "src/assets/scss/_variables.scss";`
-      }
-    }
-  }
+    plugins: [vue()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "src/assets/scss/_variables.scss";`
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    base: '/',
 })
